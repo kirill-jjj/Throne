8b856d67a316ca9ed2a8f3bcb7eef26f207c617e
kirill-jjj <kirillkolovi@gmail.com>
2026-02-15 22:10:35 +0500

    fix: always route tab/backtab to focus navigation

--- 3rdparty/qv2ray/v2/ui/QvAutoCompleteTextEdit.cpp
+++ 3rdparty/qv2ray/v2/ui/QvAutoCompleteTextEdit.cpp
@@ -65,6 +65,7 @@ namespace Qv2ray::ui::widgets {
     AutoCompleteTextEdit::AutoCompleteTextEdit(const QString &prefix, const QStringList &sourceStrings, QWidget *parent) : QPlainTextEdit(parent) {
         this->prefix = prefix;
         this->setLineWrapMode(QPlainTextEdit::NoWrap);
+        this->setTabChangesFocus(true);
         c = new QCompleter(this);
         c->setModel(new QStringListModel(sourceStrings, c));
         c->setWidget(this);
@@ -116,11 +117,11 @@ namespace Qv2ray::ui::widgets {
     }
 
     void AutoCompleteTextEdit::keyPressEvent(QKeyEvent *e) {
-        if (e->key() == Qt::Key_Tab && e->modifiers() == Qt::NoModifier) {
+        if (e->key() == Qt::Key_Tab) {
             focusNextPrevChild(true);
             return;
         }
-        if (e->key() == Qt::Key_Backtab && e->modifiers() == Qt::ShiftModifier) {
+        if (e->key() == Qt::Key_Backtab) {
             focusNextPrevChild(false);
             return;
         }
